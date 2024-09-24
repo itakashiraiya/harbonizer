a7babdfd223fc49ce8a07f998b713079f4f952c1
itakashiraiya <79550547+itakashiraiya@users.noreply.github.com>
2024-09-24 16:39:13 +0100

    1st popup test

--- lua/harbonizer/harpoon/init.lua
+++ lua/harbonizer/harpoon/init.lua
@@ -1,1 +1,13 @@
-print("hello harpoon")
+local popup = require("plenary.popup")
+
+local M = {}
+
+M.hello = function()
+	print("hello harpoon")
+end
+
+M.popup = function()
+	popup.create(1)
+end
+
+return M
