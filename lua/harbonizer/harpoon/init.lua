01ef4cf756c733afc722dee7995b9a7aca7caa97
itakashiraiya <79550547+itakashiraiya@users.noreply.github.com>
2024-09-24 16:54:39 +0100

    remove popup

--- lua/harbonizer/harpoon/init.lua
+++ lua/harbonizer/harpoon/init.lua
@@ -6,13 +6,16 @@ M.hello = function()
 	print("hello function")
 end
 
-print("hello harpoon")
+print("hello harpooner")
 
+--[[
 M.pop = function()
 	local popup_result = popup.create(1) -- Assuming popup.create returns a table or something to inspect
 
 	-- If you want to print the popup_result, use vim.inspect
 	print(vim.inspect(popup_result))
 end
+]]
+--
 
 return M
