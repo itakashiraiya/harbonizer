1b6ea2d67d7e683fe03b7aa0b86d41ffc5be58b9
Itakashiraiya <79550547+itakashiraiya@users.noreply.github.com>
2025-01-29 20:58:03 +0000

    major update to test nvim plugin

--- nvim/test.lua
+++ nvim/test.lua
@@ -1,18 +1,32 @@
 -- Notify when the plugin is loaded
-vim.notify("Plugin 'harbonizer.nvim.test' is loaded and ready!", vim.log.levels.INFO)
+vim.notify("Plugin 'harbonizer.nvim.test' is loaded and ready!!!", vim.log.levels.INFO)
 
--- Define a command to test the plugin
-vim.api.nvim_create_user_command("harbonizer.query", function()
+-- local mainDir = "default"
+-- local M = {
+-- 	init = function(mainScriptDir)
+-- 		mainDir = mainScriptDir
+-- 	end,
+-- }
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	-- Define a command to test the plugin
+	callback = function()
+		local bash = require("bash")
+		local dir = bash.getEnv("HARBONIZER_DIR")
+		bash.exec(dir .. "/init.lua test")
+		vim.notify("Plugin 'plugin-name' is running correctly!\ninit: " .. dir, vim.log.levels.INFO)
+	end,
+})
+
+vim.api.nvim_create_user_command("HarbonizerQuery", function()
 	vim.notify(
 		"Plugin 'plugin-name' is running correctly!\nserver name: " .. vim.fn.serverlist()[1],
 		vim.log.levels.INFO
 	)
 end, {})
 
-vim.api.nvim_create_user_command("harbonizer.init", function()
-	vim.notify("Plugin 'plugin-name' is running correctly!\ninit", vim.log.levels.INFO)
-end, {})
-
-vim.api.nvim_create_user_command("harbonizer.shutdown", function()
-	vim.notify("Plugin 'plugin-name' is running correctly!\nshutdown", vim.log.levels.INFO)
-end, {})
+vim.api.nvim_create_autocmd("VimLeavePre", {
+	callback = function()
+		os.execute("touch /home/viktor/dev/lua/harbonizer/tmp")
+	end,
+})
